7c8a324b7988e09c0384edc019f314a70eccdb9b
Mikhail Deyev <mdeev@neoflex.ru>
2022-01-05 19:53:22 +0300

    improve repos

--- src/main/kotlin/com/deyevs/inc/trainingsimulator/repository/AnswerRepository.kt
+++ src/main/kotlin/com/deyevs/inc/trainingsimulator/repository/AnswerRepository.kt
@@ -2,5 +2,7 @@ package com.deyevs.inc.trainingsimulator.repository
 
 import com.deyevs.inc.trainingsimulator.model.Answer
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AnswerRepository : JpaRepository<Answer, Long>
