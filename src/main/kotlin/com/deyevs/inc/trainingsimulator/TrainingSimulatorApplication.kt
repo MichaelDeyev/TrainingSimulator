0594a2b01f7022d4068cd2ef35b938d4fd017b61
Mikhail Deyev <mdeev@neoflex.ru>
2022-01-05 14:51:59 +0300

    Initial commit

---
+++ src/main/kotlin/com/deyevs/inc/trainingsimulator/TrainingSimulatorApplication.kt
@@ -0,0 +1,11 @@
+package com.deyevs.inc.trainingsimulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainingSimulatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrainingSimulatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/deyevs/inc/trainingsimulator/TrainingSimulatorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.deyevs.inc.trainingsimulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainingSimulatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
